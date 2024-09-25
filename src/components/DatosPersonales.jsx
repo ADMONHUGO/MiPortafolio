179ec03aee90099446c99be1d92ddf6686faf487
Hugo Sanchez Mora <hsanchez_business@hotmail.com>
2024-09-24 18:31:09 -0600

    Se ajusto el diseño responsivo para moviles

--- src/components/DatosPersonales.jsx
+++ src/components/DatosPersonales.jsx
@@ -18,7 +18,7 @@ const DatosPersonales = () => {
                     <div className={estilos.textos}>
                         <p className={estilos.tituloNombre}>Mi nombre es Hugo Sánchez Mora</p>
                         <p className={estilos.textoDescriptivo}>Soy una persona que le gusta el desarrollo de técnologias, así como implementar y participar en proyectos nuevos, donde pueda demostrar mis conocimientos que he adquirido con el paso del tiempo.</p>
-                        <p className={estilos.textoDescriptivo}>De igual forma me agrada el estar aprendiendo las nuevas técnologias para poder aplicarlas en el desarrollo de mi trabajo.</p>
+                        <p className={estilos.textoDescriptivo}>Me gusta seguir aprendiendo las nuevas técnologias para poder aplicarlas en el desarrollo de mi trabajo.</p>
                         <p className={estilos.bienvenidos}>Bienvenidos a mi portafolio como desarrollador FULLSTACK</p>
                         <p className={estilos.carrera}><FaDochub fontSize={18} /> Ingeniero en Sistemas Computacionales</p>
                         <p className={estilos.correo}><FaAt fontSize={18}/> Hugo.Sanchez.Mora@hotmail.com</p>
@@ -41,9 +41,9 @@ const DatosPersonales = () => {
                         <div className={estilos.redesSociales}>
                             
                                 <a href="https://github.com/ADMONHUGO" className={estilos.margen} target='_blanck' title='GitHub' ><FaGithub fontSize={35} /></a>  
-                                <a href="www.linkedin.com/in/hugo-sanchez-mora135790" target='_blanck' title='LINKEDIN' ><FaLinkedin  fontSize={35} /></a>
+                                <a href="https://www.linkedin.com/in/hugo-sanchez-mora135790/" target='_blanck' title='LINKEDIN' ><FaLinkedin  fontSize={35} /></a>
                         
-                            
+                                
                         </div>
 
                     </div>
