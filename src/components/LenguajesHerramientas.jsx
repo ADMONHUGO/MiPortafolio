ff804e91f69889e67f6e605077e21493c9c2a3e3
Hugo Sanchez Mora <hsanchez_business@hotmail.com>
2025-05-22 01:14:03 -0600

    Se agrego animaciones a las imagenes

--- src/components/LenguajesHerramientas.jsx
+++ src/components/LenguajesHerramientas.jsx
@@ -3,8 +3,7 @@ import logoPython from '../imgs/pythonLogo.png';
 import logoDjango from '../imgs/djangoLogo.png';
 import logoJavaScript from '../imgs/javascriptLogo.png';
 import logoReact from '../imgs/reactLogo.webp';
-import logoHTML5 from '../imgs/html5Logo.png';
-import logoCSS3 from '../imgs/css3Logo.png';
+import Git from '../imgs/git.png';
 import logoPostgresql from '../imgs/postgresqlLogo.webp';
 import logoPostman from '../imgs/postmanLogo.png';
 import logoRestApi from '../imgs/restapiLogo.png';
@@ -64,61 +63,63 @@ const LenguajeHerramientasConponent = () => {
                 </div>
             </div>
             <div id='contenedorGrafico' >
+                    <h4 className={estilos.subTitulo}>Desarrollo Frontend</h4>
                 <div className={estilos.contenedor}>
-                    <div className={estilos.item} title='Python'>
-                            <img className={estilos.iconos} src={logoPython} alt="Python"/>
-                    </div>
+                    
                     <div className={estilos.item} title='Django'>
                         <img className={estilos.iconosv2} src={logoDjango} alt="Django" />
-                    </div>
-                    <div className={estilos.item} title='JavaScript'>
-                        <img className={estilos.iconos} src={logoJavaScript} alt="JavaScript" />
                     </div>
                     <div className={estilos.item} title='React JS'>
                         <img className={estilos.iconos} src={logoReact} alt="ReactJs" />
                     </div>
-                    <div className={estilos.item} title='HTML 5'>
-                        <img className={estilos.iconos} src={logoHTML5} alt="HTML5" />
+                    <div className={estilos.item} title='Bootstrap'>
+                        <img className={estilos.iconos} src={logoBoostrap} alt="Bootstrap" />
                     </div>
-                    <div className={estilos.item} title='Css 3'>
-                        <img className={estilos.iconos} src={logoCSS3} alt="CSS3" />
+                </div>
+                <h4 className={estilos.subTitulo}>Desarrollo Backend</h4>
+                <div className={estilos.contenedor}>
+                    <div className={estilos.item} title='Python'>
+                        <img className={estilos.iconos} src={logoPython} alt="Python"/>
                     </div>
+                    <div className={estilos.item} title='JavaScript'>
+                        <img className={estilos.iconos} src={logoJavaScript} alt="JavaScript" />
+                    </div>
+                    <div className={estilos.item} title='Node JS'>
+                        <img className={estilos.iconosv3} src={logoNodeJS} alt="NodeJs" />
+                    </div>
+                    <div className={estilos.item} title='Express'>
+                        <img className={estilos.iconos} src={logoExpressJS} alt="ExpressJs" />
+                    </div>
+                    <div className={estilos.item} title='NPM'>
+                        <img className={estilos.iconosv2} src={logoNPM} alt="NPM" />
+                    </div>
+                    <div className={estilos.item} title='API REST'>
+                        <img className={estilos.iconosv2} src={logoRestApi} alt="API Rest" />
+                    </div>
+                    <div className={estilos.item} title='Postman'>
+                        <img className={estilos.iconosv3} src={logoPostman} alt="Postman" />
+                    </div>
+                </div>
+                <h4 className={estilos.subTitulo}>Base de datos, entre otros</h4>
+                <div className={estilos.contenedor}>
+                    <div className={estilos.item} title='Linux'>
+                        <img className={estilos.iconosv2} src={logoLinux} alt="Linux" />
+                    </div>
+                    <div className={estilos.item} title='Git'>
+                        <img className={estilos.iconos} src={Git} alt="Git" />
+                    </div>
+                    <div className={estilos.item} title='PL/SQL'>
+                        <img className={estilos.iconosv2} src={logoPLSQL} alt="PL/SQL" />
+                    </div>
+                    
                     <div className={estilos.item} title='PostgreSQL'>
                         <img className={estilos.iconos} src={logoPostgresql} alt="Postgresql" />
                     </div>
                     <div className={estilos.item} title='Metodología SCRUM'>
                         <img className={estilos.iconos} src={logoScrum} alt="Scrum" />
                     </div>
-                    <div className={estilos.item} title='Postman'>
-                        <img className={estilos.iconosv3} src={logoPostman} alt="Postman" />
-                    </div>
-                    <div className={estilos.item} title='API REST'>
-                        <img className={estilos.iconosv2} src={logoRestApi} alt="API Rest" />
-                    </div>
-                    <div className={estilos.item} title='GitHub'>
-                        <img className={estilos.iconos} src={logoGitHub} alt="GitHub" />
-                    </div>
-                    <div className={estilos.item} title='NPM'>
-                        <img className={estilos.iconosv2} src={logoNPM} alt="NPM" />
-                    </div>
                 </div>
-                <div className={estilos.contenedor}>
-                    <div className={estilos.item} title='Linux'>
-                        <img className={estilos.iconosv2} src={logoLinux} alt="Linux" />
-                    </div>
-                    <div className={estilos.item} title='Node JS'>
-                        <img className={estilos.iconosv3} src={logoNodeJS} alt="NodeJs" />
-                    </div>
-                    <div className={estilos.item} title='Express JS'>
-                        <img className={estilos.iconos} src={logoExpressJS} alt="ExpressJs" />
-                    </div>
-                    <div className={estilos.item} title='PL/SQL'>
-                        <img className={estilos.iconosv2} src={logoPLSQL} alt="PL/SQL" />
-                    </div>
-                    <div className={estilos.item} title='Bootstrap'>
-                        <img className={estilos.iconos} src={logoBoostrap} alt="Bootstrap" />
-                    </div>
-                </div>
+
             </div>
             <div id='contenedorTexto'>
                 <div className={estilos.fila}>
@@ -162,26 +163,7 @@ const LenguajeHerramientasConponent = () => {
                             </div>
                         </div>
                     </div>
-                    <div className={estilos.columna}>
-                        <div className={estilos.card}>
-                            <div className={estilos.cardTitle} >
-                                <span>HTML 5</span>
-                            </div>
-                            <div className={estilos.cardBody}>
-                                <span>Nivel: Intermedio</span>
-                            </div>
-                        </div>
-                    </div>
-                    <div className={estilos.columna}>
-                        <div className={estilos.card}>
-                            <div className={estilos.cardTitle} >
-                                <span>CSS 3</span>
-                            </div>
-                            <div className={estilos.cardBody}>
-                                <span>Nivel: Intermedio</span>
-                            </div>
-                        </div>
-                    </div>
+                    
                 </div>
                 <div className={estilos.fila}>
                     <div className={estilos.columna}>
@@ -200,7 +182,7 @@ const LenguajeHerramientasConponent = () => {
                                 <span>SCRUM</span>
                             </div>
                             <div className={estilos.cardBody}>
-                                <span>Nivel: Basico</span>
+                                <span>Nivel: Básico</span>
                             </div>
                         </div>
                     </div>
@@ -210,7 +192,7 @@ const LenguajeHerramientasConponent = () => {
                                 <span>POSTMAN</span>
                             </div>
                             <div className={estilos.cardBody}>
-                                <span>Nivel: Basico</span>
+                                <span>Nivel: Básico</span>
                             </div>
                         </div>
                     </div>
@@ -227,7 +209,7 @@ const LenguajeHerramientasConponent = () => {
                     <div className={estilos.columna}>
                         <div className={estilos.card}>
                             <div className={estilos.cardTitle} >
-                                <span>GitHub</span>
+                                <span>Git</span>
                             </div>
                             <div className={estilos.cardBody}>
                                 <span>Nivel: Intermedio</span>
@@ -252,7 +234,7 @@ const LenguajeHerramientasConponent = () => {
                                 <span>Linux</span>
                             </div>
                             <div className={estilos.cardBody}>
-                                <span>Nivel: basico</span>
+                                <span>Nivel: Básico</span>
                             </div>
                         </div>
                     </div>
